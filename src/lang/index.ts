02539f70dafcbb3dad8031369fabdacfeaf678f2
zhanglx <zhanglx@guohe.org>
2021-08-02 17:25:42 +0800

    20210802

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,10 +1,8 @@
 import { createI18n } from 'vue-i18n'
 import { getLanguage } from '@/utils/cookies'
-
 // element-ui built-in lang
 import elementEnLocale from 'element-plus/lib/locale/lang/en'
 import elementZhLocale from 'element-plus/lib/locale/lang/zh-cn'
-
 // User defined lang
 import enLocale from './en'
 import zhLocale from './zh'
